8db9a9dff33454268e80bd712ab1d13b7639dfbe
Randy <dengyijun2007@163.com>
2018-05-28 16:02:45 +0800

    Solve bug by moving hidden state into GPU

--- model.py
+++ model.py
@@ -38,9 +38,9 @@ class DecoderRNN(nn.Module):
     
     def forward(self, features, captions):
         # Initialize hidden state each forward step, shape: [num_layers, batch, hidden_size]
-        self.hidden = (torch.zeros(self.num_layers, features.shape[0], self.hidden_size), torch.zeros(self.num_layers, features.shape[0], self.hidden_size))
+        self.hidden = (torch.zeros(self.num_layers, features.shape[0], self.hidden_size).to("cuda" if torch.cuda.is_available() else "cpu"), torch.zeros(self.num_layers, features.shape[0], self.hidden_size).to("cuda" if torch.cuda.is_available() else "cpu"))
         # Transpose captions shape from [batch, seq_len] to [seq_len, batch]
-        captions_transposed = captions.permute(1, 0)
+        captions_transposed = captions.permute(1, 0).contiguous()
         # Words embedding
         embeds = self.word_embeddings(captions_transposed)
         # Joint the features from images with the embedded captions, chape: [seq_len, batch, embed_size] 
@@ -54,7 +54,7 @@ class DecoderRNN(nn.Module):
         # Softmax scores
         tag_scores = F.log_softmax(tag_space, dim=2)
         # Transpose tag_scores shape from [seq_len, batch, vocab_size] to [batch, seq_len, vocab_size]
-        return tag_scores.permute(1, 0, 2)
+        return tag_scores.permute(1, 0, 2).contiguous()
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
